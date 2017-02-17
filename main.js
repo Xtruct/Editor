f593f0effa15d965ea0946d53cd13b3fe58ce2df
Quentin GOINAUD <quentin.goinaud@epitech.eu>
2017-02-17 13:14:37 +0100

    fixed bug electron not running correctly on linux

--- main.js
+++ main.js
@@ -1,76 +1,84 @@
-const electron      = require('electron');
-const app           = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const isDev         = require('electron-is-dev');
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const isDev = require('electron-is-dev');
 
-const path = require('path');
-const url  = require('url');
+const path = require('path')
+const url = require('url')
 
-let mainWindow;
+let mainWindow
 
 app.commandLine.appendSwitch('--force-device-scale-factor', '1');
 app.commandLine.appendSwitch('--disable-gpu');
 
-
 app.setAppUserModelId("com.editor.xtruct");
 
-//TODO asar extract ressources
-
 function createWindow() {
-	let screen = electron.screen;
 
-	const {width, height} = screen.getPrimaryDisplay().workAreaSize;
-	mainWindow            = new BrowserWindow({
-		title          : "Xtruct Editor",
-		protocol       : 'file:',
-		slashes        : true,
-		icon           : path.join(__dirname, 'build/icon.ico'),
-		backgroundColor: '#2e2c29',
-		width          : width,
-		height         : height
-	});
+    let screen = electron.screen;
 
-	mainWindow.loadURL(url.format(path.join(__dirname, 'index.html')));
+    const {
+        width,
+        height
+    } = screen.getPrimaryDisplay().workAreaSize;
 
-	mainWindow.setMenu(null);
+    mainWindow = new BrowserWindow({
+        title: "Xtruct Editor",
+        icon: path.join(__dirname, 'build/icon.ico'),
+        backgroundColor: '#2e2c29',
+        width: width,
+        height: height
+    });
 
-	mainWindow.maximize();
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-	process.argv.forEach(function (value) {
-		console.log(value);
-		if (value === "-dev")
-			mainWindow.webContents.openDevTools();
-	});
+    mainWindow.setMenu(null);
 
-	if (isDev) {
-		mainWindow.webContents.openDevTools();
-		console.log('Running in development');
-		//let client        = require('electron-connect').client;
-		//client.create(mainWindow);
-	} else {
-		console.log('Running in production');
-	}
+    mainWindow.maximize();
 
-	mainWindow.on('closed', function () {
-		mainWindow = null;
-		app.quit();
-	});
+    process.argv.forEach(function(value) {
+        console.log(value);
+        if (value === "-dev")
+            mainWindow.webContents.openDevTools();
+    });
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+        console.log('Running in development');
+        //let client        = require('electron-connect').client;
+        //client.create(mainWindow);
+    } else {
+        console.log('Running in production');
+    }
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+        app.quit();
+        console.log('quit');
+    });
 }
 
-app.on('browser-window-created', function (e, window) {
-	window.setMenu(null);
+app.on('browser-window-created', function(e, window) {
+    window.setMenu(null);
+    console.log('quit');
 });
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
-app.on('window-all-closed', function () {
-	//if (process.platform !== 'darwin') {
-	app.quit();
-	//}
+app.on('window-all-closed', function() {
+    //if (process.platform !== 'darwin') {
+    app.quit();
+    //}
 });
 
-app.on('activate', function () {
-	if (mainWindow === null) {
-		createWindow();
-	}
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow();
+        console.log('quit');
+    }
 });
